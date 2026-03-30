7340e62bf48ad45a118acb709ebdd6aff13809e1
Aniruddhakharve <kharveayash@gmail.com>
2026-03-31 02:41:11 +0530

    added terraform file to automate ec2 instance launching in aws

---
+++ ec2.tf
@@ -0,0 +1,73 @@
+#Region: ap-south-1
+provider "aws" {
+  region = "ap-south-1"
+}
+
+#key value pair
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "terra-automated-key"
+  public_key = file("terra-automated-key.pub") #interpolating the public key file
+}
+
+#creating Default VPC
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+#creating Security Group
+resource "aws_security_group" "my_security_group" {
+  name        = "terra-security-group"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_default_vpc.default.id  #interpolating the default vpc id
+
+  tags = {
+    Name = "terra-security-group"
+  }
+}
+
+#creating Ingress rule for Security Group which is inbound rule
+resource "aws_vpc_security_group_ingress_rule" "my_inbound_rule_http" {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = aws_default_vpc.cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "my_inbound_rule_ssh" {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = aws_default_vpc.cidr_block
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+#creating Egress rule for Security Group which is outbound rule
+resource "aws_vpc_security_group_egress_rule" "my_outbound_rule_all" {
+  security_group_id = aws_security_group.my_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+#creating EC2 instance
+resource "aws_instance" "my_instance" {
+  ami           = "ami-05d2d839d4f73aafb"
+
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [aws_security_group.my_security_group.id] #interpolating the security group id
+
+  key_name = aws_key_pair.my_key_pair.key_name #interpolating the key pair name
+  
+  #Root Block storage for your EC2 instance
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "my-terraform-automated-instance"
+  }
+}
