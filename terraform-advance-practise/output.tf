52b8e0622f70a6a0b20ce2de85d51f8408bdbb4a
Aniruddhakharve <kharveayash@gmail.com>
2026-04-01 01:50:27 +0530

    op

--- terraform-advance-practise/output.tf
+++ terraform-advance-practise/output.tf
@@ -9,7 +9,7 @@ output "ec2_public_dns" {
 }
 
 
-output "ec2_public_ip" {
+output "ec2_public_ip_existing_instance" {
     value = aws_instance.my_existing_instance.public_ip
     description = "This output will display the public IP address of the EC2 instance"
 }
