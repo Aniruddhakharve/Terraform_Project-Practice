07651885aa79874fb1ba894412202272e3161fb0
Aniruddhakharve <kharveayash@gmail.com>
2026-04-01 00:34:24 +0530

    added the backend to use in my terraform.tf file

--- terraform-advance-practise/terraform.tf
+++ terraform-advance-practise/terraform.tf
@@ -5,4 +5,10 @@ terraform {
       version = "6.38.0"
     }
   }
+  backend "s3" {
+    bucket = "my-terraform-backend-bucket-dhurandaar"
+    dynamodb_table = "my-terraform-backend-table"
+    key = "terraform.tfstate"
+    region = "ap-south-1"
+  }
 }
