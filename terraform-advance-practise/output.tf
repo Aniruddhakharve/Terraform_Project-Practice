703c7c1d0577f50e684891c2625946316cf0175d
Aniruddhakharve <kharveayash@gmail.com>
2026-04-01 01:49:09 +0530

    op

--- terraform-advance-practise/output.tf
+++ terraform-advance-practise/output.tf
@@ -7,3 +7,9 @@ output "ec2_public_dns" {
     value = aws_instance.my_instance.public_dns
     description = "This output will display the public DNS name of the EC2 instance"
 }
+
+
+output "ec2_public_ip" {
+    value = aws_instance.my_existing_instance.public_ip
+    description = "This output will display the public IP address of the EC2 instance"
+}
